2f59764ad7be91db320ea602d1551aa8508e1dfa
Braxton Talbert <105814852+braxtontalbert@users.noreply.github.com>
2024-06-13 18:16:15 -0400

    Added comments and docs to JutsuEntry class

--- Jutsu/JutsuEntry.cs
+++ Jutsu/JutsuEntry.cs
@@ -9,18 +9,35 @@ using Debug = UnityEngine.Debug;
 
 namespace Jutsu
 {
+    /**
+     * Jutsu Entry - Entry point to the Jutsu mod
+     * CustomData class from ThunderRoad
+     * Use this to import VFX GameObjects, or for more complex things such as voice activation
+     */
     public class JutsuEntry : CustomData
     {
+        //static reference for Singleton structure
         public static JutsuEntry local;
+        
+        //Reference to global coroutine manager (Useful for non monbehaviour classes)
         public GameObject coroutine = new GameObject();
         public CoroutineManager coroutineManager;
+        
+        //Speech Recognition Engine object
         private SpeechRecognitionEngine recognizer;
+        
+        //Gameobject for Substitution Jutsu
         internal ItemData logData;
+        
+        //GameObjects for Shadow Possession Jutsu
         public GameObject shadow;
         public GameObject shadowSFX;
+        
+        //GameObjects for Chidori 
         public GameObject chidori;
         public GameObject chidoriStartSFX;
         public GameObject chidoriLoopSFX;
+        
         public override void OnCatalogRefresh()
         {
             //Only want one instance of the loader running
@@ -34,7 +51,10 @@ namespace Jutsu
         {
             await Task.Run(() =>
             {
+                //Add new component of Coroutine Manager to coroutine manager reference
                 coroutineManager = coroutine.AddComponent<CoroutineManager>();
+                
+                //Setup speech recognition with choice words
                 Choices jutsu = new Choices();
                 jutsu.Add("Substitution");
                 recognizer = new SpeechRecognitionEngine();
@@ -44,7 +64,11 @@ namespace Jutsu
                 recognizer.SetInputToDefaultAudioDevice();
                 recognizer.RecognizeAsync(RecognizeMode.Multiple);
                 recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+                
+                //Prevents game from getting hung up when using speech recognition engine.
                 Application.quitting += () => Process.GetCurrentProcess().Kill();
+                
+                //get GameObjects for VFX or SFX for jutsus
                 logData = Catalog.GetData<ItemData>("JutsuLog");
                 Catalog.LoadAssetAsync<GameObject>("apoz123.Jutsu.YinRelease.VFX.ShadowPosession",
                     gameobject => { shadow = gameobject;}, "ShadowPossesion");
