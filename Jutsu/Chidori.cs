61c10f9ad83dc6e2b36b979a28e97991fdb2f681
Braxton Talbert <105814852+braxtontalbert@users.noreply.github.com>
2024-06-21 12:00:58 -0400

    Updated chidori to pierce properly.

--- Jutsu/Chidori.cs
+++ Jutsu/Chidori.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine.VFX;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.VFX;
 
 namespace Jutsu
 {
@@ -8,9 +11,10 @@ namespace Jutsu
     /**
      * Class for Chidori spell
      */
-    public class Chidori : SpellCastCharge
+    public class Chidori : SkillData
     {
-        private GameObject chidori;
+        private ItemData chidoriData;
+        private Item chidori;
         private VisualEffect chidoriVFX;
         private GameObject chidoriStart;
         private GameObject chidoriLoop;
@@ -21,100 +25,162 @@ namespace Jutsu
         private bool damager = false;
         private Damager pierce;
         private GameObject pierceGO;
-        
-        /**
-         * Fire
-         * parameters - active (boolean)
-         * Runs code for setting up Chidori assets
-         */
-        public override void Fire(bool active)
+        private bool chidoriStarted = false;
+        private bool vfxStarted = false;
+        private Coroutine chidoriRun;
+        public override void OnSkillLoaded(SkillData skillData, Creature creature)
         {
-            //Execute once
-            if (!chidori)
-            {
-                //Instantiate all Chidori GameObjects
-                //Set up references for Chidori VFX and SFX
-                chidori = GameObject.Instantiate(JutsuEntry.local.chidori);
-                chidoriVFX = chidori.GetComponentInChildren<VisualEffect>();
-                chidoriStart = GameObject.Instantiate(JutsuEntry.local.chidoriStartSFX);
-                chidoriStartSFX = chidoriStart.GetComponent<AudioSource>();
-                chidoriLoop = GameObject.Instantiate(JutsuEntry.local.chidoriLoopSFX);
-                chidoriLoopSFX = chidoriLoop.GetComponent<AudioSource>();
-                chidoriVFX.Play();
-                
-                //Set once
-                instantiated = true;
-            }
+            base.OnSkillLoaded(skillData, creature);
+
+            chidoriRun = GameManager.local.StartCoroutine(RunChidori());
+
+        }
+
+        public override void OnSkillUnloaded(SkillData skillData, Creature creature)
+        {
+            base.OnSkillUnloaded(skillData, creature);
+            GameManager.local.StopCoroutine(chidoriRun);
         }
 
 
-        public override void UpdateCaster()
+        private FixedJoint joint;
+        private bool hasPenetrated = false;
+        private Creature penetrated;
+        IEnumerator RunChidori()
         {
-            //Run if all references are assigned
-            if (instantiated)
+            yield return new WaitForSeconds(2f);
+            while (true)
             {
-                
-                //While spell active and holding trigger
-                if (spellCaster.isFiring)
+                if (Player.local.creature.handRight.playerHand.isFist)
                 {
-                    //Check to see if sound is played
-                    if (!startSoundPlayed)
+                    if (!chidori && !chidoriStarted)
                     {
-                        chidoriStartSFX.Play();
-                        startSoundPlayed = true;
-                    }
-                    //Check to see if sound can begin looping
-                    if ((startSoundPlayed && !chidoriStartSFX.isPlaying) && !chidoriLoopSFX.isPlaying)
-                    {
-                        chidoriLoopSFX.Play();
-                    }
-                    
-                    //Get SpellCaster magic transform position and set chidori to that (Adjusted some to go into the hand, rather than in the palm)
-                    //Update position and rotation every frame
-                    var transform = spellCaster.ragdollHand.caster.magicSource.transform;
-                    chidori.transform.position = transform.position + (-transform.forward * 0.05f);
-                    chidori.transform.rotation = transform.rotation;
-                    
-                    /*
-                     * Currently not functional code
-                     */
-                    /*if (!damager)
-                    {
-                        damager = true;
-                        GameObject go = new GameObject();
-                        go.AddComponent<Damager>();
-                        pierceGO = GameObject.Instantiate(go);
-                        pierce = pierceGO.GetComponent<Damager>();
-                        pierceGO.transform.parent = spellCaster.ragdollHand.fingerMiddle.tip.transform;
-                        pierce.direction = Damager.Direction.Forward;
-                        pierce.penetrationLength = 1f;
-                        pierce.transform.position = spellCaster.ragdollHand.fingerMiddle.tip.transform.position;
-                        pierce.transform.rotation = spellCaster.ragdollHand.fingerMiddle.tip.transform.rotation;
+                        chidoriStarted = true;
+                        //Instantiate all Chidori GameObjects
+                        //Set up references for Chidori VFX and SFX
+                        Catalog.TryGetData<ItemData>("ChidoriItem", out chidoriData, true);
+                        if (chidoriData != null)
+                        {
+                            chidoriData.SpawnAsync(item =>
+                            {
+                                chidori = item;
+                                chidori.physicBody.useGravity = false;
+                                chidori.transform.position =
+                                    Player.local.handRight.ragdollHand.transform.position;
+                                chidori.transform.rotation =
+                                    Player.local.handRight.ragdollHand.transform.rotation;
+                                chidori.IgnoreRagdollCollision(Player.local.creature.ragdoll);
+                                joint = Player.local.handRight.ragdollHand.gameObject.AddComponent<FixedJoint>();
+                                joint.breakForce = Mathf.Infinity;
+                                joint.breakTorque =  Mathf.Infinity;
+                                joint.connectedBody = chidori.physicBody.rigidBody;
+                                
+                                foreach (Creature c in Creature.allActive)
+                                {
+                                    if (!c.isPlayer)
+                                    {
+                                        Player.local.creature.ragdoll.rightUpperArmPart.ragdoll.IgnoreCollision(
+                                            c.ragdoll, true);
+                                        Player.local.creature.handRight.ragdoll.IgnoreCollision(c.ragdoll, true);
+                                    }
+                                }
+                                chidoriVFX = chidori.gameObject.GetComponentInChildren<VisualEffect>();
+                                chidoriStart = GameObject.Instantiate(JutsuEntry.local.chidoriStartSFX);
+                                chidoriStartSFX = chidoriStart.GetComponent<AudioSource>();
+                                chidoriLoop = GameObject.Instantiate(JutsuEntry.local.chidoriLoopSFX);
+                                chidoriLoopSFX = chidoriLoop.GetComponent<AudioSource>();
+                                chidoriVFX.Stop();
+                            });
+                        }
+                        else
+                        {
+                            chidoriStarted = false;
+                        }
 
-                    }*/
+                    }
+
+                    else if(chidori)
+                    {
+                        //While spell active and holding trigger
+                            //Check to see if sound is played
+                            if (!startSoundPlayed)
+                            {
+                                chidoriStartSFX.Play();
+                                startSoundPlayed = true;
+                            }
+
+                            //Check to see if sound can begin looping
+                            if ((startSoundPlayed && !chidoriStartSFX.isPlaying) && !chidoriLoopSFX.isPlaying)
+                            {
+                                chidoriLoopSFX.Play();
+                            }
+
+                            //Get SpellCaster magic transform position and set chidori to that (Adjusted some to go into the hand, rather than in the palm)
+                            //Update position and rotation every frame
+                            if (!vfxStarted)
+                            {
+                                chidoriVFX.Play();
+                                vfxStarted = true;
+                            }
+
+                            if (chidori.isPenetrating)
+                            {
+                                penetrated = chidori.GetComponentInParent<Creature>();
+                                hasPenetrated = true;
+                            }
+                            if (!chidori.isPenetrating && hasPenetrated)
+                            {
+                                Debug.Log("HAS PENETRATED IS TRUE");
+                                GameObject.DestroyImmediate(joint);
+                                chidori.transform.position =
+                                    Player.local.handRight.ragdollHand.transform.position;
+                                chidori.transform.rotation =
+                                    Player.local.handRight.ragdollHand.transform.rotation;
+                                chidori.IgnoreRagdollCollision(Player.local.creature.ragdoll);
+                                chidori.IgnoreRagdollCollision(penetrated.ragdoll);
+                                Debug.Log("JOINT IS: " + joint);
+                                joint = Player.local.handRight.ragdollHand.gameObject.AddComponent<FixedJoint>();
+                                joint.breakForce = Mathf.Infinity;
+                                joint.breakTorque =  Mathf.Infinity;
+                                joint.connectedBody = chidori.physicBody.rigidBody;
+                                chidori.ResetRagdollCollision();
+                                chidori.IgnoreRagdollCollision(Player.local.creature.ragdoll);
+                                hasPenetrated = false;
+                            }
+                    }
                 }
                 else
                 {
                     //Reset all values to default while still updating Chidori position
-                    startSoundPlayed = false;
-                    chidoriVFX.Stop();
-                    chidori.transform.position = spellCaster.ragdollHand.caster.magicSource.transform.position;
-                    chidori.transform.rotation = spellCaster.ragdollHand.caster.magicSource.transform.rotation;
+                    if (chidori && vfxStarted)
+                    {
+                        startSoundPlayed = false;
+                        if (startSoundPlayed)
+                        {
+                            chidoriStartSFX.Stop();
+                        }
+                        else
+                        {
+                            chidoriLoopSFX.Stop();
+                        }
+                        chidoriVFX.Stop();
+                        chidoriStarted = false;
+                        vfxStarted = false;
+                        foreach (Creature c in Creature.allActive)
+                        {
+                            if (!c.isPlayer)
+                            {
+                                Player.local.creature.ragdoll.rightUpperArmPart.ragdoll
+                                    .IgnoreCollision(c.ragdoll, false);
+                                Player.local.creature.handRight.ragdoll.IgnoreCollision(c.ragdoll, false);
+                            }
+                        }
+                        chidori.Despawn();
+                    }
                 }
-            }
-        }
 
-        public override void Unload()
-        {
-            
-            //Remove assets when spell is unloaded
-            base.Unload();
-            GameObject.Destroy(chidori);
-            GameObject.Destroy(chidoriVFX);
-            GameObject.Destroy(chidoriStart);
-            GameObject.Destroy(chidoriStartSFX);
-            GameObject.Destroy(chidoriLoop);
-            GameObject.Destroy(chidoriLoopSFX);
+                yield return null;
+            }
         }
     }
 }
--- Jutsu/JutsuEntry.cs
+++ Jutsu/JutsuEntry.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Speech.Recognition;
 using System.Threading.Tasks;
@@ -20,22 +21,24 @@ namespace Jutsu
         public static JutsuEntry local;
         
         //Reference to global coroutine manager (Useful for non monbehaviour classes)
-        public GameObject coroutine = new GameObject();
+        //public GameObject coroutine = new GameObject();
         //public CoroutineManager coroutineManager;
-        public SDFBakeTool bakeTool = new GameObject().AddComponent<SDFBakeTool>();
+        public SDFBakeTool bakeTool; // = new GameObject().AddComponent<SDFBakeTool>();*/
         
         //Speech Recognition Engine object
-        private SpeechRecognitionEngine recognizer;
+        //private SpeechRecognitionEngine recognizer;
         
         //Gameobject for Substitution Jutsu
         internal ItemData logData;
+
+        internal ItemData chidoriItemData;
         
         //GameObjects for Shadow Possession Jutsu
         public GameObject shadow;
         public GameObject shadowSFX;
         
         //GameObjects for Chidori 
-        public GameObject chidori;
+        //public GameObject chidori;
         public GameObject chidoriStartSFX;
         public GameObject chidoriLoopSFX;
         //VFX for Vacuum Blade
@@ -50,56 +53,39 @@ namespace Jutsu
             
         }
 
+        public override IEnumerator LoadAddressableAssetsCoroutine()
+        {
+            AsyncSetup();
+            return base.LoadAddressableAssetsCoroutine();
+            
+        }
+
         async void AsyncSetup()
         {
             await Task.Run(() =>
             {
                 //coroutineManager = coroutine.AddComponent<CoroutineManager>();
                 //Add new component of Coroutine Manager to coroutine manager reference
-                Choices jutsu = new Choices();
-                jutsu.Add("Substitution");
-                recognizer = new SpeechRecognitionEngine();
-                Grammar servicesGrammar = new Grammar(new GrammarBuilder(jutsu));
-                recognizer.RequestRecognizerUpdate();
-                recognizer.LoadGrammarAsync(servicesGrammar);
-                recognizer.SetInputToDefaultAudioDevice();
-                recognizer.RecognizeAsync(RecognizeMode.Multiple);
-                recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
-                
                 //Prevents game from getting hung up when using speech recognition engine.
                 Application.quitting += () => Process.GetCurrentProcess().Kill();
                 
                 //get GameObjects for VFX or SFX for jutsus
-                logData = Catalog.GetData<ItemData>("JutsuLog");
+                /*logData = Catalog.GetData<ItemData>("JutsuLog");
                 Catalog.LoadAssetAsync<GameObject>("apoz123.Jutsu.YinRelease.VFX.ShadowPosession",
                     gameobject => { shadow = gameobject;}, "ShadowPossesion");
                 Catalog.LoadAssetAsync<GameObject>("apoz123.Jutsu.YinRelease.VFX.ShadowPossessionJutsu.SFX",
-                    go => { shadowSFX = go;}, "ShadowPossessionSFX");
-                Catalog.LoadAssetAsync<GameObject>("apoz123.LightningStyle.Chidori", go => { chidori = go;}, "ChidoriVFX");
-                Catalog.LoadAssetAsync<GameObject>("apoz123.LightningStyle.Chidori.ChidoriStartSFX", go => { chidoriStartSFX = go;}, "ChidoriStartSFX");
-                Catalog.LoadAssetAsync<GameObject>("apoz123.LightningStyle.Chidori.ChidoriLoopSFX",
+                    go => { shadowSFX = go;}, "ShadowPossessionSFX");*/
+                //Catalog.LoadAssetAsync<GameObject>("apoz123.LightningStyle.Chidori", go => { chidori = go;}, "ChidoriVFX");
+                GameManager.local.StartCoroutine(Catalog.LoadAddressableAssetsCoroutine());
+               Catalog.LoadAssetAsync<GameObject>("SOTSP.Jutsu.LightningRelease.Chidori.SFX.start", go => { chidoriStartSFX = go;}, "ChidoriStartSFX");
+                Catalog.LoadAssetAsync<GameObject>("SOTSP.Jutsu.LightningRelease.Chidori.SFX.loop",
                     go => { chidoriLoopSFX = go;}, "ChidoriLoopSFX");
-                Catalog.LoadAssetAsync<GameObject>("apoz123.Jutsu.WindRelease.VFX.VacuumBlade", data =>{this.vacuumBlade = data;}, "VacuumBladeVFX");
-                Catalog.LoadAssetAsync<GameObject>("debugObject", data =>{this.debugObject = data;}, "DebugObject");
+
+               // GameManager.local.StartCoroutine(loadAssetCoroutine);
+                //GameManager.local.StartCoroutine(assetCoroutine);
+                /*Catalog.LoadAssetAsync<GameObject>("apoz123.Jutsu.WindRelease.VFX.VacuumBlade", data =>{this.vacuumBlade = data;}, "VacuumBladeVFX");*/
+                /*Catalog.LoadAssetAsync<GameObject>("debugObject", data =>{this.debugObject = data;}, "DebugObject");*/
             });
-        }
-
-        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-        {
-            if (e.Result.Text != null && e.Result.Confidence > 0.93f)
-            {
-                Debug.Log("Jutsu Result: " + e.Result.Text);
-                Debug.Log("Jutsu Confidence: " + e.Result.Confidence);
-
-                if (e.Result.Text.Equals("Substitution"))
-                {
-                    if (!Player.currentCreature.gameObject.GetComponent<SubstitutionJutsu>())
-                    {
-                        Player.currentCreature.gameObject.AddComponent<SubstitutionJutsu>();
-                    }
-                }
-
-            }
         }
     }
     
